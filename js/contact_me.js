2b98e29e1efe9bb0aa217453f4f9b7528687324b
Hugo Andres Orozco <hugandoro@gmail.com>
2019-05-14 16:44:35 -0500

    Estructura definitiva - Web version 1.0
    Genus Group SAS - Pagina web institucional

--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("GRACIAS ! :) Su mensaje a sido enviado y en breve uno de nuestros asesores se pondra en contacto");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -46,7 +46,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Lo sentimos " + firstName + ", al parecer estamos presentando una falla tecnica, por favor intentelo nuevamente, gracias."));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
